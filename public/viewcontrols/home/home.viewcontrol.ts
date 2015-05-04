b36824331c15e7a6207bc9b890321e82eac6ff2b
Benjamin MICHEL <benjamin.michel@ensimag.fr>
2015-05-04 23:41:43 +0200

    Platypi challenge app

---
+++ public/viewcontrols/home/home.viewcontrol.ts
@@ -0,0 +1,40 @@
+﻿/// <reference path="../../_references.d.ts" />
+
+import plat = require('platypus');
+import BaseViewControl = require('../base/base.viewcontrol');
+import UserRepository = require('../../repositories/user/user.repository');
+import OrderViewControl = require('../order/order.viewcontrol');
+import ProductsService = require('../../services/products/products.service');
+
+class HomeViewControl extends BaseViewControl {
+    constructor(private userRepository: UserRepository, private productsService: ProductsService) {
+        super();
+    }
+
+    templateString: string = require('./home.viewcontrol.html');
+
+    context: any = {
+        products: <Array<models.IProduct>>[]
+    };
+
+    navigatedTo() {
+        this.productsService.getProducts().then((products) => {
+            this.context.products = products;
+        });
+    }
+
+    canNavigateTo() {
+        if(this.userRepository.userid === 0) {
+            this.navigator.navigate('login-vc');
+            return false;
+        }
+    }
+
+    order(id: number) {
+        this.navigator.navigate(OrderViewControl, { parameters: { id: id } });
+    }
+}
+
+plat.register.viewControl('home-vc', HomeViewControl, [UserRepository, ProductsService]);
+
+export = HomeViewControl;
